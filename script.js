366a75548695063848399551ddae8a64455323ea
subhadeep <subhadeepc19@gmail.com>
2021-06-02 16:51:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const query = document.getElementById("search")
+
+const button = document.querySelector("button")
+
+button.addEventListener("click", () => {
+    // console.log(query.value)
+    query.value = "bruh"
+})
